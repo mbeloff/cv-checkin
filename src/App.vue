c35aba4d2edd6e372bfaeac1271e815856d90d02
michael beloff <michael.a.beloff@gmail.com>
2022-04-07 15:58:48 +1000

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup></script>
 
 <template>
-  <div>
-    <header class="h-20 bg-gray-200">Campervan Village</header>
-    <main class="bg-gray-400 min-h-screen">
+  <div id="app" class="max-w-screen grid min-h-screen">
+    <header
+      class="fixed flex h-20 w-full items-center bg-gray-900 p-10 shadow-xl"
+    >
+      <p class="text-2xl font-bold text-blue-500">Campervan Village</p>
+    </header>
+    <main class="min-h-screen bg-gray-100 pt-20">
       <router-view />
     </main>
   </div>
 </template>
+
+<style lang="postcss">
+#app {
+  font-family: "Poppins", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.my-input {
+  @apply flex-1 rounded border bg-gray-200 py-3 pl-2;
+}
+
+.group:hover .form-i {
+  @apply text-yellow-500;
+}
+</style>
